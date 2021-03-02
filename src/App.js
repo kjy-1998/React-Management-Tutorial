511e2fb9a089ae556ac2bc3a7b7b90caa7607bc2
jaeyunkim <jaeyun.kim@telstar-hommel.com>
2021-03-02 19:39:19 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [
+  {
+    id: 1,
+    image: 'https://placeimg.com/64/64/1',
+    name: '김재윤',
+    birthday: '980721',
+    gender: '남자',
+    job: '개발자',
+  },
+  {
+    id: 2,
+    image: 'https://placeimg.com/64/64/2',
+    name: '이도윤',
+    birthday: '9801221',
+    gender: '남자',
+    job: '예비개발자',
+  },
+  {
+    id: 3,
+    image: 'https://placeimg.com/64/64/3',
+    name: '홍정우',
+    birthday: '9801231',
+    gender: '남자',
+    job: '엘리베이터 현장소장',
+  }
+];
 
 function App() {
   return (
-    <div className="bg--gray">
-      <img src={logo} alt="logo"/>
-      <h2>Let's develop management system!</h2>
-    </div>
+    <React.Fragment>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              job={c.job}/>
+          );
+        })
+      }
+    </React.Fragment>
   );
 }
 
